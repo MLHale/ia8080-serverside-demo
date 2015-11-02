b6c8a23a9a584592077d998e676d18610628c2ce
MLHale <mlhale@unomaha.edu>
2015-11-02 21:07:52 +0000

    On step 14 of developing the lab2 tutorial

--- api/models.py
+++ api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
 
 # Create your models here.
+class Tag(models.Model):
+    """
+    This is a tag for user submitted post entries.
+    """
+    name = models.CharField(max_length=20, blank=False, unique=True)
+    
+    class Meta:
+        verbose_name_plural = "Tags"
+  
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name','id')
+        
+class Forumpost(models.Model):
+    """
+    This is a forum post for storing user submitted post entries.
+    """
+    title = models.CharField(max_length=40, blank=False)
+    content = models.TextField(blank=False)
+    author = models.ForeignKey(User)
+    promoted = models.BooleanField(default=False)
+    likes = models.IntegerField(default=0)
+    tags = models.ManyToManyField('tag', blank=True)
+    
+    class Meta:
+        #This will be used by the admin interface      
+        verbose_name_plural = "Forumposts"
+  
+class ForumpostAdmin(admin.ModelAdmin):
+    #This inner class indicates to the admin interface how to display a post
+    #See the Django documentation for more information
+    list_display = ('title', 'content', 'author', 'promoted', 'likes')
--- api/views.py
+++ api/views.py
@@ -1,3 +1,93 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Import models
+from django.db import models
+from django.contrib.auth.models import *
+from myapp.api.models import *
+
+#REST API
+from rest_framework import viewsets
+from myapp.api.serializers import *
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+#needed if you want to use all class-based views, replace foo and bar with respective names (e.g. forumposts-list)
+# class APIRoot(APIView):
+#     def get(self, request):
+#         # Assuming we have views named 'foo-view' and 'bar-view'
+#         # in our project's URLconf.
+#         return Response({
+#             'foo': reverse('foo-view', request=request),
+#             'bar': reverse('bar-view', request=request)
+#         })
+
+#Viewset forum post example
+# class ForumpostViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows for CRUD operations on forumpost objects.
+#     """
+#     queryset = Forumpost.objects.all()
+#     serializer_class = ForumpostSerializer
+
+
+#more detailed, but more control class based view example
+class ForumpostList(APIView):
+    """
+    List all forum posts, or create a new post.
+    """
+    def get(self, request, format=None):
+        posts = Forumpost.objects.all() #you could limit this to only the posts for which the user has access
+        serializer = ForumpostSerializer(posts, many=True, context={'request': request})
+        return Response(serializer.data) #you can customize the response here
+    
+    def post(self, request, format=None):
+        serializer = ForumpostSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) #you could customize the response here
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #you could customize the error message here
+    
+
+class ForumpostDetail(APIView):
+    """
+    Retrieve, update or delete a single forum post.
+    """
+    def get_object(self, pk):
+        try:
+            return Forumpost.objects.get(pk=pk)
+        except Forumpost.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = ForumpostSerializer(post, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = ForumpostSerializer(post, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows for CRUD operations on tag objects.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
